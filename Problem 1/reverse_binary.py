bf82144009cc4ebff3b454754b846564b4f559e9
DanteCastilleja <dsc2386@utulsa.edu>
2024-02-21 14:27:27 -0600

    Problem 1

--- Problem 1/reverse_binary.py
+++ Problem 1/reverse_binary.py
@@ -1,15 +1,23 @@
 """
 Complete the following python code to print the reverse binary representation of positive integer number entered by the user.
 
-Name:
-Lab Time:
+Name: Dante Castilleja
+Lab Time: Friday 3:00
 
 """
 
 
 def reverse_binary():
     user_num = int(input())
-    # YOUR CODE HERE
+    while user_num >= 1 :
+        if user_num % 2 == 0 :
+            print(0, end = "")
+            user_num //= 2
+        elif user_num % 2 != 0 :
+            print(1, end = "")
+            user_num //= 2
+        elif user_num <= 1 :
+            print(1, end = "")
 
 if __name__ == "__main__":
     reverse_binary()

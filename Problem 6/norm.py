a3f1826c2968ec9ad7292f17bdbe40ec59edb058
DanteCastilleja <dsc2386@utulsa.edu>
2024-02-22 22:48:30 -0600

    Problem 6

--- Problem 6/norm.py
+++ Problem 6/norm.py
@@ -1,12 +1,21 @@
 """
 Complete the following python code to take in a list of values from the user and then normalize them
 
-Name:
-Lab Time:
+Name: Dante Castilleja
+Lab Time: Friday 3:00
 """
 
 def norm():
-    # Write your code here
+    num = int(input())
+    data = []
+    for _ in range(num):
+        point = float(input())
+        data.append(point)
+        maxdata = max(data)
+    for point in data:
+        newdata = point / maxdata
+        print(f'{newdata:.2f}')
+
 
 if __name__ == "__main__":
     norm()
